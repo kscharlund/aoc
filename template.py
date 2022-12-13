e2a0744fe691789838fe57a1a7b5f1fb41bb5dd5
Karl Schärlund <kalle@scharlund.se>
2022-12-13 12:09:06 +0100

    Basic setup

---
+++ template.py
@@ -0,0 +1,26 @@
+import sys
+from pprint import pprint
+import math
+
+
+def get_data():
+    return []
+
+
+def a(data):
+    pass
+
+
+def b(data):
+    pass
+
+
+def main():
+    data = get_data()
+    a(data)
+    print()
+    b(data)
+
+
+if __name__ == "__main__":
+    main()
